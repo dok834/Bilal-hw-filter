cd5b53c9f711692f2131ff153ceb69105b6ba55b
dok834 <evgenez43@gmail.com>
2024-10-30 18:58:31 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+    //     Д/з тема 'filter'
+    
+    
+    //Номер = 1
+// const g = [1, 2, 3, 4, 5, 6]
+// const new1 = g.filter((item)=>{
+//     if(item % 2 == 0){
+//         return true
+//     }
+// }) 
+// console.log(new1)
+
+    //Номер = 2
+// const str = ['привет', 'мир']
+// const newstr = str.map((item)=>{
+//     return item.toUpperCase()
+// })
+// console.log(newstr)
+
+    //Номер = 3
+//const number = [1,2,3,4]
+//const newnumber = number.map((item)=>{
+//    return item + ' '
+//})
+//console.log(newnumber)
+
+    //Номер = 4
+// const g =  ['кот', 'собака', 'тигр']
+// const new1 = g.map((item)=>{
+//     return item +'!'
+    
+// })
+// console.log(new1)
+
+
+
+
+    //Номер = 5
+// const users = [
+//     { name: "Alice", role: "admin" },
+//     { name: "Bob", role: "user" },
+//     { name: "Charlie", role: "admin" },
+//     { name: "Dave", role: "user" }
+//   ];
+
+// const newname = users.map((item)=>{
+//     return item.name
+// })
+// console.log(newname)
